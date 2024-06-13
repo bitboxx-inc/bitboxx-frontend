8ed0716783e95608a5bdb026b2793860afc0588b
digiot <digiot.abc@gmail.com>
2024-06-14 03:53:27 +0900

    レタースペース機能を追加

---
+++ src/infras/LetterSpacing.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+    export let fontSize: number;
+    export let spacingRatio: number;
+    const calculateSpacing = (size: number, ratio: number) => size * (ratio / 100);
+</script>
+
+<span style={`font-size: ${fontSize}px`} class="{`tracking-[${fontSize}px] mr-[-${calculateSpacing(fontSize, spacingRatio)}px]`}">
+  <slot></slot>
+</span>
