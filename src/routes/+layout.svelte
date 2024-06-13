35c55231037291426c6910a28ae07ad63314c429
digiot <digiot.abc@gmail.com>
2024-06-14 02:51:39 +0900

    SSG サイトテンプレート

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,29 @@
+<script lang="ts">
+	import '../app.css';
+
+	// 画面全体の最大幅を決める
+	const width = 1448;
+</script>
+
+<div class={`max-w-[${width}px] boxx`} style="margin: 0 auto;">
+	<slot />
+</div>
+
+<svelte:head>
+
+	<!-- Fonts -->
+	<link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+	<link
+		rel="stylesheet"
+		href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,600,700"
+	/>
+
+	<!-- Material Typography -->
+	<link
+		rel="stylesheet"
+		href="https://unpkg.com/@material/typography@14.0.0/dist/mdc.typography.css"
+	/>
+
+	<!-- SMUI -->
+	<link rel="stylesheet" href="https://unpkg.com/svelte-material-ui/bare.css" />
+</svelte:head>
